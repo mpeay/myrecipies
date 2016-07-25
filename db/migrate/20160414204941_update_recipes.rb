3daeab1212721fc92c092eccd8112c32f010ec60
Michael Peay <mike+cloud9@peay.us>
2016-07-25 20:57:22 +0000

    create chefs and recipes association and tests

--- db/migrate/20160414204941_update_recipes.rb
+++ db/migrate/20160414204941_update_recipes.rb
@@ -1,4 +1,9 @@
 class UpdateRecipes < ActiveRecord::Migration
   def change
+    create_table :recipes do |t|
+      t.string :name
+      t.text :summary
+      t.text :description
+      t.timestamps
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160414205029) do
+ActiveRecord::Schema.define(version: 20160725203617) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160414205029) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "chef_id"
   end
 
   create_table "users", force: :cascade do |t|
